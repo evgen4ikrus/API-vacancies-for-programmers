1bdd156558bfb6bb71b8e3fc1ce8b2cb2bb974ad
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-07-14 12:43:37 +0300

    Оптимизировал код, удалил лишнее

--- main.py
+++ main.py
@@ -13,9 +13,20 @@ def predict_rub_salary(vacancy):
             elif salary['to']:
                 return salary['to'] * 0.8
 
-    
+
+def get_vacancies_information(url, language, area=1, date_from=30):
+    params = {
+            'text': f'Программист {language}',
+            'area': area,
+            'date_from': date_from,
+        }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    return response.json()
+
+
 def main():
-    
+
     popular_programming_languages = [
         'Python',
         'Java',
@@ -33,41 +44,26 @@ def main():
     popular_vacancies_statistics = {}
 
     for language in popular_programming_languages:
-        params = {
-            'text': f'Программист {language}',
-            'area': '1',
-            'date_from': date_from,
-        }
-        response = requests.get(url, params=params)
-        response.raise_for_status()
+        vacancies_information = get_vacancies_information(url, language, date_from=date_from)
 
-        vacancies = response.json()['items']
+        vacancies = vacancies_information['items']
+        vacancy_counts = vacancies_information['found']
         total_salary = 0
         vacancies_processed = 0
-        vacancy_counts = response.json()['found']
-        
+
         for vacancy in vacancies:
             salary = predict_rub_salary(vacancy)
             if salary:
                 vacancies_processed += 1
                 total_salary += salary
-        
+
         popular_vacancies_statistics[language] = {
             'vacancies_found': vacancy_counts,
             'vacancies_processed': vacancies_processed,
             'average_salary': int(total_salary / vacancies_processed),
         }
-            
-    params = {
-        'text': f'Программист Python',
-        'area': '1',
-        'date_from': date_from,
-    }
-    response = requests.get(url, params=params)
-    response.raise_for_status()
 
     print(popular_vacancies_statistics)
-    
 
 if __name__ == '__main__':
     main()
