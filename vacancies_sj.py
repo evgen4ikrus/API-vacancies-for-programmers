ceda74d0287ce9beae4a78710404d8d9e7dc6d1b
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-07-15 13:20:29 +0300

    Добавил функцию get_vacancies_sj_information

--- vacancies_sj.py
+++ vacancies_sj.py
@@ -4,6 +4,20 @@ import os
 from api_halpers import predict_salary
 
 
+def get_vacancies_sj_information(secret_key):
+    url = 'https://api.superjob.ru/2.0/vacancies/'
+    headers = {
+        'X-Api-App-Id': secret_key,
+    }
+    params = {
+        'catalogues': 48,
+        'town': 'Москва'
+    }
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    return response.json()['objects']
+
+
 def predict_rub_salary_for_superJob():
     pass
 
@@ -16,21 +30,10 @@ def main():
 
     load_dotenv()
     super_job_secret_key = os.getenv("SUPER_JOB_SECRET_KEY")
-
-    url = 'https://api.superjob.ru/2.0/vacancies/'
-    headers = {
-        'X-Api-App-Id': super_job_secret_key,
-    }
-    params = {
-        'catalogues': 48,
-        'town': 'Москва'
-    }
-    response = requests.get(url, headers=headers, params=params)
-    response.raise_for_status()
-    vacancies_information = response.json()
     
-    vacancies_information = response.json()['objects']
-    for vacancy in vacancies_information:
+    vacancies_sj_information = get_vacancies_sj_information(super_job_secret_key)
+    
+    for vacancy in vacancies_sj_information:
         print(f"{vacancy['profession']}, {vacancy['town']['title']}")
         
         
